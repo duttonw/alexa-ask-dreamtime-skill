5aec8b772c78213b800ff52466606c90cbcdf33b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-03 06:24:01 +0000

    updated lambda/constants.js.

--- lambda/constants.js
+++ lambda/constants.js
@@ -5,17 +5,31 @@ exports.skill = {
   dynamoDBTableName: 'dreamtime',
 };
 
+/* This outputs 2021-07-02 if today is 2021-07-03, this is the format they use for their audio file*/
+function playerdate() {
+    var d = new Date((new Date()).valueOf() - 1000*60*60*24);
+    var month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+    
+}
+
 exports.audioData = [
   {
-    title: 'Bedtime Stories: Johnny Huckle, Miffy&#39;s Adventures Big and Small, Play School, Kids listen Bookshelf',
-    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kbe-2021-07-02.m4a/master.m3u8',
+    title: 'Bedtime Stories',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kbe-' + playerdate() + '.m4a/master.m3u8',
   },
   {
     title: 'Lullabies',
-    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/klu-2021-07-02.m4a/master.m3u8',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/klu-' + playerdate() + '.m4a/master.m3u8',
   },
   {
     title: 'Dream Time',
-    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kdt-2021-07-02.m4a/master.m3u8',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kdt-' + playerdate() + '.m4a/master.m3u8',
   },
 ];
