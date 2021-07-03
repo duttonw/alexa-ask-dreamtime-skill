e3b8147ea1d719d056e8b5659748026dd799f639
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-03 05:15:40 +0000

    updated lambda/constants.js.

---
+++ lambda/constants.js
@@ -0,0 +1,21 @@
+/* CONSTANTS */
+
+exports.skill = {
+  appId: '',
+  dynamoDBTableName: 'dreamtime',
+};
+
+exports.audioData = [
+  {
+    title: 'Bedtime Stories: Johnny Huckle, Miffy&#39;s Adventures Big and Small, Play School, Kids listen Bookshelf',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kbe-2021-07-02.m4a/master.m3u8',
+  },
+  {
+    title: 'Lullabies',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/klu-2021-07-02.m4a/master.m3u8',
+  },
+  {
+    title: 'Dream Time',
+    url: 'https://abcradiomodhls-vh.akamaihd.net/i/kidslisten/audio/kdt-2021-07-02.m4a/master.m3u8',
+  },
+];
