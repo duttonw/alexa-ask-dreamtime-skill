86620a7189c37393a9493669b9734cf12d7e5545
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-06 04:14:58 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -691,5 +691,5 @@ const skillBuilder = Alexa.SkillBuilders.custom();
             dynamoDBClient: new AWS.DynamoDB({apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION})
           })
       )
-      .withCustomUserAgent('dreamtime/v1.2')
+      .withCustomUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Alexa/Dreamtime_v1.0')
       .lambda();
